b5a8405c3699d2cb820a4c2a810adca10eaba043
aes_minhnd <balota.dev01@outlook.com>
2024-09-26 09:52:21 +0700

    update lowns

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // index.js
-
-
-import cors from 'cors';
-import express from 'express'
-import puppeteer from 'puppeteer';
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const cors = require('cors');
+const puppeteer = require('puppeteer');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -12,36 +13,36 @@ app.use(express.json());
 
 // API để crawl dữ liệu
 app.post('/api/crawl', async (req, res) => {
-  const { url } = req.body;
-  if (!url) {
-    return res.status(400).send({ error: 'URL is required' });
-  }
+    const { url } = req.body;
+    if (!url) {
+        return res.status(400).send({ error: 'URL is required' });
+    }
 
-  try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
 
-    // Truy cập vào URL và chờ tải xong
-    await page.goto(url);
+        // Truy cập vào URL và chờ tải xong
+        await page.goto(url);
 
-    // Lấy văn bản từ div có class là list-group-item list-group-item-info
-    const textContent = await page.evaluate(() => {
-      const elements = document.querySelectorAll('.list-group-item.list-group-item-info');
-      return Array.from(elements).map(element => element.innerText);
-    });
+        // Lấy văn bản từ div có class là list-group-item list-group-item-info
+        const textContent = await page.evaluate(() => {
+            const elements = document.querySelectorAll('.list-group-item');
+            return Array.from(elements).map(element => element.innerText);
+        });
 
-    // Đóng trình duyệt
-    await browser.close();
+        // Đóng trình duyệt
+        await browser.close();
 
-    // Trả về nội dung văn bản
-    res.send({ text: textContent });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({ error: 'Error crawling the URL' });
-  }
+        // Trả về nội dung văn bản
+        res.send({ text: textContent });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: 'Error crawling the URL' });
+    }
 });
 
 // Khởi động server
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
